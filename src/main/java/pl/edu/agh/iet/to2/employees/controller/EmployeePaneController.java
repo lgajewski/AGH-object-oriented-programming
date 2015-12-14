e727855a4121ea17de81f1bd004ae42d1180299f
Lukasz Gajewski <lukasz.gajewski94@gmail.com>
2015-12-14 23:03:22 +0100

    Maven structure of project

--- src/main/java/pl/edu/agh/iet/to2/employees/controller/EmployeePaneController.java
+++ src/main/java/pl/edu/agh/iet/to2/employees/controller/EmployeePaneController.java
@@ -2,9 +2,11 @@ package pl.edu.agh.iet.to2.employees.controller;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
@@ -17,6 +19,7 @@ import pl.edu.agh.iet.to2.employees.model.IEmployee;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class EmployeePaneController {
 
@@ -24,15 +27,19 @@ public class EmployeePaneController {
 
     @FXML
     public TextField searchField;
+    public ComboBox filterComboBox;
 
     @FXML
     private ListView<EmployeeItem> employeeListView;
 
     private EmployeeGenerator employeeGenerator = new EmployeeGenerator();
+    private ObservableList<EmployeeItem> observableList;
 
     @FXML
     private void initialize() throws IOException {
-        ObservableList<EmployeeItem> observableList = createObservableEmployeeItemList(AMOUNT_OF_EMPLOYEES);
+        observableList = FXCollections.observableArrayList();
+
+        populateObservableListWithEmployeeItems(AMOUNT_OF_EMPLOYEES);
 
         employeeListView.setCellFactory(new Callback<ListView<EmployeeItem>, ListCell<EmployeeItem>>() {
             @Override
@@ -41,7 +48,7 @@ public class EmployeePaneController {
                     @Override
                     protected void updateItem(EmployeeItem item, boolean empty) {
                         super.updateItem(item, empty);
-                        if(item != null) {
+                        if (item != null) {
                             setGraphic(item.getPane());
                         }
                     }
@@ -52,8 +59,7 @@ public class EmployeePaneController {
         employeeListView.setItems(observableList);
     }
 
-    private ObservableList<EmployeeItem> createObservableEmployeeItemList(int amount) throws IOException {
-        ObservableList<EmployeeItem> observableList = FXCollections.observableArrayList();
+    private void populateObservableListWithEmployeeItems(int amount) throws IOException {
         List<IEmployee> employeeList = employeeGenerator.generate(amount);
 
         for (IEmployee employee : employeeList) {
@@ -64,11 +70,31 @@ public class EmployeePaneController {
             EmployeeCellController controller = loader.getController();
             observableList.add(new EmployeeItem(employee, employeeCell, controller));
         }
-        return observableList;
     }
 
     public void handleSearchEvent(Event event) {
+        String filterText = searchField.getText();
+        String checkedItem = (String) filterComboBox.getValue();
 
+        if (filterText.length() > 0) {
+            FilteredList<EmployeeItem> filteredList = observableList.filtered(employeeItem -> {
+                IEmployee employee = employeeItem.getEmployee();
+                switch (checkedItem) {
+                    case "Name":
+                        return employee.getName().contains(filterText);
+                    case "Surname":
+                        return employee.getSurname().contains(filterText);
+                    default:
+                        return false;
+                    //TODO - Occupation may be null
+                    //case "Occupation":
+                    //return employee.getDetails().
+                }
+            });
+            employeeListView.setItems(filteredList);
+        } else {
+            employeeListView.setItems(observableList);
+        }
     }
 
     class EmployeeItem {
@@ -89,9 +115,13 @@ public class EmployeePaneController {
 
             EmployeeDetails details = employee.getDetails();
 
-            if(details != null) {
+            if (details != null) {
                 controller.setEmployeeOccupation(details.getOccupation().getName());
             }
+        }
+
+        public IEmployee getEmployee() {
+            return employee;
         }
 
         public AnchorPane getPane() {
