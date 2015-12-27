fa206b6b0d7ef05f2315dc1063c3e8ea90da124f
Lukasz Gajewski <lukasz.gajewski94@gmail.com>
2015-12-27 02:32:37 +0100

    Updated dependencies between modules

--- app/src/main/java/pl/edu/agh/iet/to2/App.java
+++ app/src/main/java/pl/edu/agh/iet/to2/App.java
@@ -5,6 +5,10 @@ import javafx.stage.Stage;
 
 public class App extends Application {
 
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Company Management System");
