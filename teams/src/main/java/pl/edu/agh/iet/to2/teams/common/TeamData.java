7ae8e6c350a3b03bc9d869a654ade1d6445d239d
Izabela <isabs@cmoki.pl>
2016-01-23 16:22:26 +0100

    TeamData: Complete, ready to tests

--- teams/src/main/java/pl/edu/agh/iet/to2/teams/common/TeamData.java
+++ teams/src/main/java/pl/edu/agh/iet/to2/teams/common/TeamData.java
@@ -8,12 +8,15 @@ import pl.edu.agh.iet.to2.teams.db.SqlHelper;
 import java.util.List;
 
 public class TeamData {
-    //all that matters is below
-    //you can use methods which are over this, but please continue my tree below and after that delete all trashes
-    //i have created not implemented method below to you, so that you can continue it
-    //~Iz
 
-    public boolean getTeamsTree(TeamsModelManipulator teamsModelManipulator) throws Exception {
+    private TeamsModelManipulator teamsModelManipulator;
+
+    public TeamData(TeamsModelManipulator teamsModelManipulator) {
+        this.teamsModelManipulator = teamsModelManipulator;
+    }
+
+
+    public boolean getTeamsTree() throws Exception {
         String query1 = "SELECT * FROM Manager WHERE parentManagerId=NULL";
         List<List> rs = SqlHelper.getResultSet(query1, 3);
         if(rs.size()==0) return false;
@@ -22,12 +25,12 @@ public class TeamData {
         }
         else {
             long id = (long)rs.get(0).get(1);
-            createManager(teamsModelManipulator, 0, id);
+            createManager(0, id);
             return true;
         }
     }
 
-    private void createManager(TeamsModelManipulator teamsModelManipulator, long parentId, long personId) {
+    private void createManager( long parentId, long personId) {
         String getPerson = "SELECT * FROM Person WHERE personId=" + personId;
         List<List> person =  SqlHelper.getResultSet(getPerson, 4);
         TeamManager tm = new TeamManager(personId, person.get(0).get(1).toString(),person.get(0).get(2).toString() );
@@ -40,20 +43,20 @@ public class TeamData {
         List<List> managerSubordinates = SqlHelper.getResultSet(getManagerSubordinates, 3);
 
         for(List manager : managerSubordinates){
-            createManager(teamsModelManipulator, personId, (long) manager.get(1));
+            createManager(personId, (long) manager.get(1));
         }
 
         String getTeamSubordinates = "SELECT * FROM Team WHERE managerId=" + managerId;
         List<List> teamSubordinates = SqlHelper.getResultSet(getTeamSubordinates, 3);
 
         for(List team : teamSubordinates){
-            createTeam(teamsModelManipulator, personId, (long) team.get(0));
+            createTeam(personId, (long) team.get(0));
         }
 
         //return tm;
     }
 
-    private void createTeam(TeamsModelManipulator teamsModelManipulator, long parentId, long teamId) {
+    private void createTeam(long parentId, long teamId) {
         String getTeam = "SELECT * FROM Team WHERE teamId="+teamId;
         List<List> teamsList = SqlHelper.getResultSet(getTeam, 3);
 
@@ -66,11 +69,11 @@ public class TeamData {
         List<List> membersList = SqlHelper.getResultSet(members, 3);
 
         for( List member : membersList){
-            createMember(teamsModelManipulator, teamId, (long) member.get(1));
+            createMember(teamId, (long) member.get(1));
         }
     }
 
-    private void createMember(TeamsModelManipulator teamsModelManipulator, long teamId, long personId) {
+    private void createMember(long teamId, long personId) {
         String getMember = "SELECT * FROM Person WHERE personId="+personId;
         List<List> members = SqlHelper.getResultSet(getMember, 3);
 
@@ -78,10 +81,5 @@ public class TeamData {
 
         teamsModelManipulator.addMember(teamId,testerPerson );
     }
-
-    //najpierw managerow
-    //potem po kolei po nich (przechodzenie po drzewie)
-    //i do samego dołu
-
 }
 
