696185cd2854ee36863087774b3f1698340b9320
Izabela <isabs@cmoki.pl>
2016-01-23 14:40:40 +0100

    Deleting trash methods

--- teams/src/main/java/pl/edu/agh/iet/to2/teams/common/TeamData.java
+++ teams/src/main/java/pl/edu/agh/iet/to2/teams/common/TeamData.java
@@ -1,67 +1,11 @@
 package pl.edu.agh.iet.to2.teams.common;
 
-import pl.edu.agh.iet.to2.teams.api.person.Manager;
 import pl.edu.agh.iet.to2.teams.api.person.TeamManager;
-import pl.edu.agh.iet.to2.teams.api.team.Team;
 import pl.edu.agh.iet.to2.teams.db.SqlHelper;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class TeamData {
-    Set<Team> teams;
-    Set<DBManager> managers;
-
-
-    public Set<Team> getAllTeamsFromDb(){
-        String query = "SELECT * FROM TEAM";
-        List<List> rs = SqlHelper.getResultSet(query, 3);
-
-        Set teams = new HashSet<Team>();
-
-        for(List row : rs){
-            Team team = Team.createTeam();
-            team.setId((long) row.get(0));
-            team.setName(row.get(1).toString());
-            team.setProject(null);
-            //team.setManager(getManager((long) row.get(2)));
-            teams.add(team);
-        }
-        return teams;
-    }
-
-
-    public class DBManager{
-        long id;
-        long superiorId;
-        // id = person Id, we don't need specific "manager Id"
-
-        public DBManager(long id, long superiodId){
-            this.id = id;
-            this.superiorId = superiodId;
-        }
-    }
-
-    public Set<Manager> getAllManagersFromDb(){
-        String query = "SELECT * FROM Manager";
-        List<List> rs = SqlHelper.getResultSet(query, 3);
-
-        DBManager manager;
-
-        // obtain ids
-        for(List row : rs){
-            manager = new DBManager((long)row.get(1), //personId
-                    (long)row.get(2));//parentManagerId
-            managers.add(manager);
-        }
-
-        // obtain subordinates/superiors
-
-
-        return null;
-    }
-
     //all that matters is below
     //you can use methods which are over this, but please continue my tree below and after that delete all trashes
     //i have created not implemented method below to you, so that you can continue it
