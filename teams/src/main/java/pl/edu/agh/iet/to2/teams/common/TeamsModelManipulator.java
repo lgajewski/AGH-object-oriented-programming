6ed2968cc299708ff210a0f76897dd41c4ce93b2
maciek <losiu717@poczta.onet.pl>
2016-01-26 13:26:28 +0100

    Manipulator does not log changes to database

--- teams/src/main/java/pl/edu/agh/iet/to2/teams/common/TeamsModelManipulator.java
+++ teams/src/main/java/pl/edu/agh/iet/to2/teams/common/TeamsModelManipulator.java
@@ -44,7 +44,7 @@ public class TeamsModelManipulator {
                 mainController.addController(TeamController.createControllerOn(team, mainController.getPane(), (TeamView) mainController.getView("TeamView")));
                 //team.setManager(parent);
                 parent.addTeam(team);
-                database.addEmptyTeam(team.getName(), parentId);
+               // database.addEmptyTeam(team.getName(), parentId);
                 return true;
             }
         }
@@ -91,7 +91,7 @@ public class TeamsModelManipulator {
             else{
                 mainController.addController(TeamManagerController.createControllerOn(teamManager, mainController.getPane(), (TeamView) mainController.getView("TeamView")));
                 teamsTree.setRoot(teamManager);
-                database.addManagerInDb(teamManager.getName(), teamManager.getOccupation(), 0);
+               // database.addManagerInDb(teamManager.getName(), teamManager.getOccupation(), 0);
                 view.redrawRoot(teamManager);
                 return true;
             }
@@ -105,7 +105,7 @@ public class TeamsModelManipulator {
                 mainController.addController(TeamManagerController.createControllerOn(teamManager, mainController.getPane(),(TeamView) mainController.getView("TeamView")));
               //  teamManager.setSuperior(parent);
                 parent.addManager(teamManager);
-                database.addManagerInDb(teamManager.getName(), teamManager.getOccupation(), parentId);
+               // database.addManagerInDb(teamManager.getName(), teamManager.getOccupation(), parentId);
                 return true;
             }
         }
@@ -139,7 +139,7 @@ public class TeamsModelManipulator {
         if(team != null){
             mainController.addController(TesterPersonController.createControllerOn(testerPerson, mainController.getPane(), (TeamView) mainController.getView("TeamView")));
             team.add(testerPerson);
-            database.addMember(testerPerson.getName(), testerPerson.getOccupation(), teamId);
+           // database.addMember(testerPerson.getName(), testerPerson.getOccupation(), teamId);
             return true;
         }
         else
